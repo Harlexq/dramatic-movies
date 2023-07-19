a1d62112e66b56bad971234c9a94bb7ef1f59684
Harlexq <serhanbakir5@gmail.com>
2023-07-19 18:00:16 +0300

    lightyear

--- pages/common/header.php
+++ pages/common/header.php
@@ -1,24 +1,30 @@
 <header class="header">
-    <nav class="nav">
-        <div class="search">
-            <input type="text" placeholder="Search..">
-            <i onclick="Search()" class="fa-solid fa-magnifying-glass"></i>
-        </div>
-        <div class="user">
-            <button type="button" class="happy-btn">Happy Brtihday <i class="fa-regular fa-bell"></i></button>
-            <button type="button" class="profil-btn">Harlex <i class="fa-regular fa-user"></i></button>
-            <div class="profile">
-                <div class="profile-inner">
-                    <div class="profile-title">
-                        <p>Harlex</p>
-                    </div>
-                    <nav class="profile-nav">
-                        <a href="">
-                            <p></p>
-                        </a>
-                    </nav>
-                </div>
+    <div class="header-inner">
+        <div class="header-item">
+            <div class="navbar-brand">
+                <img src="assets/img/logo.png" alt="">
             </div>
+            <menu class="navbar">
+                <a href="index.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active'; ?>">HOME</a>
+                <a href="twshow.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'twshow.php') echo 'active'; ?>">TV SHOW</a>
+                <a href="movies.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'movies.php') echo 'active'; ?>">MOVIES</a>
+                <a href="new.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'new.php') echo 'active'; ?>">NEW</a>
+            </menu>
         </div>
-    </nav>
+        <div class="header-item-two">
+            <div class="navbar-search">
+                <input type="text" placeholder="SEARCH">
+            </div>
+            <menu class="navbar">
+                <button type="button" class="navbar-btn"><i class="bi bi-gift"></i></button>
+                <button type="button" class="navbar-btn"><i class="bi bi-bell"></i></button>
+                <button type="button" class="navbar-btn">
+                    <img src="assets/img/SerhanBakir.jpg" alt="">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                        <circle cx="7" cy="7" r="6" fill="#3BFF37" stroke="white" stroke-width="2" />
+                    </svg>
+                </button>
+            </menu>
+        </div>
+    </div>
 </header>
---
+++ twshow.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dramatic</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+
+    <?php
+    include('pages/common/header.php');
+    ?>
+
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
