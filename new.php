2ce6ebc6c12e8a8d6c4d7e4ebdd606196f1b5a90
Harlexq <serhanbakir5@gmail.com>
2023-07-24 10:33:43 +0300

    dramatic

--- new.php
+++ new.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dramatic</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/responsive.css">
@@ -14,11 +14,14 @@
 
 <body>
 
-
     <?php
     include('pages/common/header.php');
+    include('pages/common/rectangle.php');
     ?>
 
+    <?php
+    include('pages/common/footer.php');
+    ?>
 
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="assets/js/main.js"></script>
